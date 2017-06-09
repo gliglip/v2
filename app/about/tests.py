a69e166556e949582a90e81aaaf559215a141188
Pradip Caulagi <caulagi@gmail.com>
2017-06-09 22:25:23 +0200

    Views and templates to create and edit toys (#16)
    * Forms, views and templates to create and edit toys
    
    * - Make templates extend base.html so we have navbar and footer
    - Fix tests and flake8
    - Add some tests for new endpoints

--- app/about/tests.py
+++ app/about/tests.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class UrlTest(TestCase):
 
     def test_about_template(self):
-        resp = self.client.get(reverse('about'))
+        resp = self.client.get(reverse('about:about'))
         assert resp.template_name.pop() == 'about/about.html'
 
     def test_welcome_template(self):
