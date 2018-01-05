d18e5566cd61c09159c7bb75bf8e248c478db61a
Pradip Caulagi <caulagi@gmail.com>
2018-01-05 13:21:18 +0100

    Add help text and more translations

--- app/toy/models.py
+++ app/toy/models.py
@@ -1,11 +1,12 @@
 from django.contrib.gis.db.models import GeometryField
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
 class Toy(models.Model):
     title = models.CharField(max_length=128)
     description = models.TextField()
-    location = GeometryField(srid=4326)
+    location = GeometryField(srid=4326, help_text=_('Used to find nearby users'))
 
     def __str__(self):
         return self.title
