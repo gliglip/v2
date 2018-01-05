6dc153c8193a4dbf12325dc8ec50495a52e9e4c6
Pradip Caulagi <caulagi@gmail.com>
2018-01-05 14:15:59 +0100

    Fix build

--- app/toy/models.py
+++ app/toy/models.py
@@ -1,6 +1,6 @@
 from django.contrib.gis.db.models import GeometryField
-from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Toy(models.Model):
